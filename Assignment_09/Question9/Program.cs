a138fd095b9fcc20ce6d25ca822b3f79c3a6b3bb
tejasbhivgade <tejasbhivgade2001@gmail.com>
2024-05-27 22:32:03 +0530

    Assignment 09 completed

---
+++ Assignment_09/Question9/Program.cs
@@ -0,0 +1,52 @@
+﻿using Question7;
+using Question8;
+
+namespace Question9
+{   
+
+    public class Manager: Employee
+    {
+        private double _bonus;
+
+        public double bonus
+        {
+            get { return _bonus; }
+            set { _bonus = value; }
+        }
+
+        public Manager() { }
+        public Manager(double bonus) { this.bonus = bonus; }
+
+        public void Accept()
+        {
+            base.Accept();
+            Console.WriteLine("Enter bonus: ");
+            bonus = Convert.ToDouble(Console.ReadLine());
+
+        }
+
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine("bonus= " + bonus);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " bonus= " + bonus;
+        }
+
+
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager m = new Manager();
+            m.Accept();
+            m.Print();
+        }
+    }
+}
