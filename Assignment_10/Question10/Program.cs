c53d3575fd6ac4e4dd167317acc50760883e430d
tejasbhivgade <tejasbhivgade2001@gmail.com>
2024-05-27 22:33:11 +0530

    Assignment 10 completed

---
+++ Assignment_10/Question10/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using Question7;
+using Question8;
+using Question9; 
+
+
+namespace Question10
+{
+    public class Supervisor : Employee
+    {
+        private int _subbordinates;
+
+        public int subbordinates
+        {
+            get { return _subbordinates; }
+            set { _subbordinates = value; }
+        }
+
+        public Supervisor() { }
+        public Supervisor(int subbordinates) { this.subbordinates = subbordinates; }
+
+        public void Accept()
+        {
+            base.Accept();
+            Console.WriteLine("Enter subbordinates: ");
+            subbordinates = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine("subbordinates= " + subbordinates);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "subbordinates= " + subbordinates;
+        }
+    }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Supervisor s = new Supervisor();
+                s.Accept();
+                s.Print();
+        }
+    }
+}
