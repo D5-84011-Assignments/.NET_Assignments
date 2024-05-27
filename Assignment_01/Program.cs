1d5b3e2d20c2581ca3da1f47a02bda9b18825270
tejasbhivgade <tejasbhivgade2001@gmail.com>
2024-05-27 22:11:28 +0530

    Assignment 01 completed

---
+++ Assignment_01/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Question01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
