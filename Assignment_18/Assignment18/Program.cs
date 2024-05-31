2ca9b2cb38438c068a8b240c8079ebed969ea336
tejasbhivgade <tejasbhivgade2001@gmail.com>
2024-05-31 17:15:09 +0530

    Assignment 18 completed

---
+++ Assignment_18/Assignment18/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Assignment18
+{
+    internal class Program
+    {
+        public delegate void mydelete(int a, int b);
+        public delegate void mydelete2(int a, int b);
+        static void Main(string[] args)
+        {
+
+            #region  Unicast Delegate
+            mydelete pointer = new mydelete(Math.Add);
+            pointer.Invoke(5, 7);
+            pointer = new mydelete(Math.Sub);
+            pointer.Invoke(5, 7);
+            pointer = new mydelete(Math.Multiply);
+            pointer.Invoke(5, 7);
+            pointer = new mydelete(Math.Divide);
+            pointer.Invoke(5, 7);
+            #endregion
+
+            #region Multicast Delegate
+            mydelete2 pointer2 = Math.Add;
+            pointer2 += Math.Sub;
+            pointer2 += Math.Multiply;
+            pointer2 += Math.Divide;
+            pointer2.Invoke(8, 4);
+            #endregion
+        }
+
+    }
+    class Math
+    {
+        public static void Add(int num1, int num2)
+        {
+            Console.WriteLine("Addition" + (num1 + num2));
+        }
+        public static void Sub(int num1, int num2)
+        {
+            Console.WriteLine("Sub" + (num1 - num2));
+
+        }
+        public static void Multiply(int num1, int num2)
+        {
+            Console.WriteLine("Multiply" + (num1 * num2));
+
+        }
+        public static void Divide(int num1, int num2)
+        {
+            Console.WriteLine("Divide" + (num1 / num2));
+
+        }
+
+    }
+}
